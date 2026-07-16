54bf2a002478c97ec63d047a9731e7f662acfa6e
ibrahim10sy <ibrahim10sy@gmail.com>
2026-07-16 22:50:15 +0000

    Corrige le test widget de connexion après fusion
    Le test restait bloqué sur l'écran de chargement : SharedPreferences.getInstance()
    n'a pas de handler de plateforme dans un test unitaire flutter test et bloque
    indéfiniment sans SharedPreferences.setMockInitialValues().

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,16 @@
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:unigest_app/app.dart';
 
 void main() {
   testWidgets('Affiche le formulaire de connexion', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const MyApp());
     await tester.pump();
+    await tester.pump(const Duration(milliseconds: 500));
 
     expect(find.text('Connexion'), findsWidgets);
     expect(find.byType(EditableText), findsNWidgets(2));
