53143f48d0dc99348a9a585b46c236001fff124a
ibrahim10sy <ibrahim10sy@gmail.com>
2026-07-25 13:21:32 +0000

    commit

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/foundation.dart';
 
+/// Application-level constants
 class AppConstants {
+  /// Application name
   static const String appName = 'unigest_app';
 
+  /// URL du backend Spring local utilisée par le module enseignant.
   static String get baseUrl {
     if (kIsWeb) {
       return 'http://localhost:5400';
     } else if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://192.168.1.29:5400';
+      return 'http://192.168.1.6:5400'; // IP spéciale pour accéder à l'hôte local depuis l'émulateur Android
+      // return 'http://10.0.2.2:5400'; // IP spéciale pour accéder à l'hôte local depuis l'émulateur Android
     } else {
       return 'http://localhost:5400';
     }
   }
 
+
+  /// Storage keys
   static const String tokenKey = 'auth_token';
   static const String userKey = 'user_data';
   static const String themeKey = 'app_theme';
-
+  
+  /// Route names
   static const String loginRoute = '/login';
   static const String homeRoute = '/home';
   static const String splashRoute = '/splash';
-
-  static const String appelRoute = '/appel';
-  static const String noteRoute = '/notes';
-  static const String emploiRoute = '/emploi';
-  static const String affectationsRoute = '/affectations';
-  static const String seancesRoute = '/seances';
 }
